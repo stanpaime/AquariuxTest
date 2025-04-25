01bc8e9e8213ecb21a4da9092d57cec8fa25793c
stanpaime <117434390+stanpaime@users.noreply.github.com>
2025-04-25 23:00:04 +0800

    This is my initial commit

---
+++ aquariux/src/main/java/com/aquariux/AquariuxApplication.java
@@ -0,0 +1,13 @@
+package com.aquariux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquariuxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AquariuxApplication.class, args);
+	}
+
+}
---
+++ aquariux/src/test/java/com/aquariux/AquariuxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aquariux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AquariuxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
