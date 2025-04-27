10984af2427526303960362fa1c4e2c273f3a7d5
stanpaime <117434390+stanpaime@users.noreply.github.com>
2025-04-27 19:52:51 +0800

    27/04/2025 This is my end commit.

--- aquariux/src/main/java/com/aquariux/AquariuxApplication.java
+++ aquariux/src/main/java/com/aquariux/AquariuxApplication.java
@@ -25,35 +25,4 @@ public class AquariuxApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AquariuxApplication.class, args);
 	}
-	
-	 @Bean
-	    public CommandLineRunner loadSampleData(UserRepository userRepository, 
-	                                            CryptoRepository priceRepo, 
-	                                            TransactionsService tradingService) {
-	        return args -> {
-	            User_Details user = new User_Details();
-	            user.setUserId(1L);
-	            user.setName("Alice");
-	            userRepository.save(user);
-
-	            Crypto_Details btcPrice = new Crypto_Details();
-	            btcPrice.setCryptoPair("BTCUSDT");
-	            btcPrice.setAskPrice(new BigDecimal("30000"));
-	            btcPrice.setBidPrice(new BigDecimal("29950"));
-	            btcPrice.setLastUpdated(LocalDateTime.now());
-	            priceRepo.save(btcPrice);
-
-	            Crypto_Details ethPrice = new Crypto_Details();
-	            ethPrice.setCryptoPair("ETHUSDT");
-	            ethPrice.setAskPrice(new BigDecimal("2000"));
-	            ethPrice.setBidPrice(new BigDecimal("1980"));
-	            ethPrice.setLastUpdated(LocalDateTime.now());
-	            priceRepo.save(ethPrice);
-
-	            // Now safely perform trades after everything is saved
-//	            tradingService.trade(1L, "BTCUSDT", "BUY", new BigDecimal("0.5"));
-//	            tradingService.trade(1L, "ETHUSDT", "BUY", new BigDecimal("5"));
-//	            tradingService.trade(1L, "BTCUSDT", "SELL", new BigDecimal("0.1"));
-	        };
-	    }
 }
--- aquariux/src/main/resources/data.sql
+++ aquariux/src/main/resources/data.sql
@@ -1,1 +1,14 @@
-insert into User_Details values (0, 0, 50000.00, 3L,'John');
+INSERT INTO User_Details (id, name, usdt_balance, btc_balance, eth_balance) 
+VALUES (1, 'Alice', 50000.00, 0, 0);
+
+INSERT INTO User_Details (id, name, usdt_balance, btc_balance, eth_balance) 
+VALUES (2, 'John', 50000.00, 0, 0);
+
+INSERT INTO User_Details (id, name, usdt_balance, btc_balance, eth_balance) 
+VALUES (3, 'Lotus', 50000.00, 0, 0);
+
+INSERT INTO Crypto_Details (crypto_pair, bid_price, ask_price, last_updated) 
+VALUES ('BTCUSDT', 94000.00, 93900.00, CURRENT_TIMESTAMP());
+
+INSERT INTO Crypto_Details (crypto_pair, bid_price, ask_price, last_updated) 
+VALUES ('ETHUSDT', 1806.00, 1806.00, CURRENT_TIMESTAMP());
